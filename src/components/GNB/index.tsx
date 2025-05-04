4cd60ff3e3fa246056f7a86273f4611dea262a76
fecapark <fecapark@gmail.com>
2025-05-05 00:45:28 +0900

    :lipstick: 로그인 페이지 마크업 작성

--- src/components/GNB/index.tsx
+++ src/components/GNB/index.tsx
@@ -2,7 +2,7 @@ import { Link } from '@tanstack/react-router'
 
 export const GNB = () => {
   return (
-    <div className="flex h-[60px] w-full items-center justify-between px-5">
+    <div className="flex h-[60px] w-full shrink-0 items-center justify-between px-5">
       <Link className="flex h-10 items-center" to="/">
         <img alt="logo" className="h-4.5" src="/Logo.png" />
       </Link>
--- src/routes/~login/~index.lazy.tsx
+++ src/routes/~login/~index.lazy.tsx
@@ -1,13 +1,18 @@
+import { GNB } from '@/components/GNB'
+import { GrainyBackground } from '@/routes/~login/components/GrainyBackground'
+import { LoginForm } from '@/routes/~login/components/LoginForm'
 import { createLazyFileRoute } from '@tanstack/react-router'
-
-import { GNB } from '../../components/GNB'
-import { GrainyBackground } from './components/GrainyBackground'
 
 const Login = () => {
   return (
-    <div>
-      <GNB />
+    <div className="flex h-screen flex-col">
       <GrainyBackground />
+      <GNB />
+      <div className="flex grow flex-col items-center justify-center gap-8">
+        <h1 className="block text-3xl font-bold">유어슈 이메일로 로그인</h1>
+        <LoginForm />
+        <div className="text-neutralSubtle text-sm">아직 YIS 계정이 없으신가요? 가입하기</div>
+      </div>
     </div>
   )
 }
