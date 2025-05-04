e9d7c13cc6d5112a826038dff6c10391fb3350cb
fecapark <fecapark@gmail.com>
2025-05-04 15:07:17 +0900

    :wrench: husky, lint-staged 기본 설정

---
+++ lint-staged.config.mjs
@@ -0,0 +1,21 @@
+const config = {
+  '**/*.{js,jsx,ts,tsx}': (stagedFiles) => {
+    // const filteredPatterns = ['__generated__', '.gen.']
+    const filteredPatterns = []
+
+    const filteredStagedFiles = stagedFiles
+      .filter((file) => {
+        const isGenerated = filteredPatterns.some((pattern) => {
+          return file.includes(pattern)
+        })
+        return !isGenerated
+      })
+      .join(' ')
+
+    return filteredStagedFiles
+      ? [`eslint --fix ${filteredStagedFiles}`, `prettier --write ${filteredStagedFiles}`]
+      : []
+  },
+}
+
+export default config
