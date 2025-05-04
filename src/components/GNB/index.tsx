2b5aa1b703f525805a6a449e7cd630fcac93222b
fecapark <fecapark@gmail.com>
2025-05-04 22:47:25 +0900

    :sparkles: GNB 구현

---
+++ src/components/GNB/index.tsx
@@ -0,0 +1,11 @@
+import { Link } from '@tanstack/react-router'
+
+export const GNB = () => {
+  return (
+    <div className="flex h-[60px] w-full items-center justify-between px-5">
+      <Link className="flex h-10 items-center" to="/">
+        <img alt="logo" className="h-4.5" src="/Logo.png" />
+      </Link>
+    </div>
+  )
+}
