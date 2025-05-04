f78cd8a737787640d4f4dc9626976d480a7f3373
fecapark <fecapark@gmail.com>
2025-05-04 14:57:58 +0900

    :sparkles: eslint, prettier 설정

--- eslint.config.js
+++ eslint.config.js
@@ -3,26 +3,126 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import react from 'eslint-plugin-react'
+import unusedImports from 'eslint-plugin-unused-imports'
+import eslintConfigPrettier from 'eslint-config-prettier/flat'
+import perfectionist from 'eslint-plugin-perfectionist'
+import reactCompiler from 'eslint-plugin-react-compiler'
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+const config = tseslint.config(
+  { ignores: ['dist', 'node_modules'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      reactCompiler.configs.recommended,
+      perfectionist.configs['recommended-natural'],
+      eslintConfigPrettier,
+    ],
+    files: ['**/*.{ts,mts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: { ...globals.browser },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
     },
     plugins: {
+      react,
+      'unused-imports': unusedImports,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/naming-convention': [
+        'error',
+        {
+          format: ['camelCase', 'UPPER_CASE', 'PascalCase'],
+          leadingUnderscore: 'allow',
+          selector: 'variableLike',
+        },
+        {
+          format: ['PascalCase'],
+          selector: ['typeLike', 'enumMember'],
+        },
+        {
+          format: ['camelCase', 'UPPER_CASE'],
+          leadingUnderscore: 'allow',
+          selector: 'typeProperty',
+          trailingUnderscore: 'allow',
+        },
       ],
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/no-unused-vars': 'error',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+      curly: 'error',
+      // 아래 자동정렬은 런타임 동작에 영향을 줄 수 있는 것들이에요.
+      'perfectionist/sort-enums': 'off',
+      'perfectionist/sort-objects': 'off',
+      'perfectionist/sort-maps': 'off',
+      'perfectionist/sort-modules': 'off',
+      'perfectionist/sort-imports': [
+        'error',
+        {
+          groups: [
+            'type',
+            ['builtin', 'external'],
+            'internal-type',
+            'internal',
+            ['parent-type', 'sibling-type', 'index-type'],
+            ['parent', 'sibling', 'index'],
+            'object',
+            'unknown',
+          ],
+          internalPattern: ['~/*', '@/*'],
+          newlinesBetween: 'always',
+          customGroups: {
+            type: {},
+            value: {},
+          },
+        },
+      ],
+      'react/display-name': [1, { ignoreTranspilerName: false }],
+      'react/no-unescaped-entities': 'off',
+      'react/no-unknown-property': ['error', { ignore: ['css'] }],
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/self-closing-comp': [
+        'error',
+        {
+          component: true,
+          html: true,
+        },
+      ],
+      'react-hooks/rules-of-hooks': 'error',
+      'react-compiler/react-compiler': 'warn',
+      // perfectionist의 sorting과 겹칠 수 있어서 off해요.
+      'sort-imports': 'off',
+      'object-shorthand': ['error', 'always'],
+      'no-console': ['error', { allow: ['warn', 'error'] }],
     },
-  },
+    settings: {
+      'import/external-module-folders': ['node_modules'],
+      'import/resolver': {},
+      react: {
+        version: 'detect',
+      },
+    },
+  }
 )
+
+export default config
