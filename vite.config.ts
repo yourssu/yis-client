c62591388ea05be9da84bb211690238af57740fb
fecapark <fecapark@gmail.com>
2025-05-04 20:37:25 +0900

    :wrench: tailwind 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
+import tailwindcss from '@tailwindcss/vite'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react-swc'
 
@@ -10,5 +11,6 @@ export default defineConfig({
     TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
     react(),
     tsconfigPaths(),
+    tailwindcss(),
   ],
 })
