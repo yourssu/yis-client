323c2b26148b1c3235cc0b65393cc698d61ea331
fecapark <fecapark@gmail.com>
2025-05-04 23:04:06 +0900

    :recycle: grainy 그라디언트 노이즈를 인라인 코드로 변경

--- src/routes/~login/components/GrainyBackground.tsx
+++ src/routes/~login/components/GrainyBackground.tsx
@@ -1,10 +1,35 @@
+const grainyOptions = {
+  filter: {
+    contrast: 2,
+    brightness: 2,
+  },
+  noise: {
+    size: 5000,
+  },
+}
+
+const noise = String.raw`
+<svg viewBox='0 0 ${grainyOptions.noise.size} ${grainyOptions.noise.size}' xmlns='http://www.w3.org/2000/svg'>
+  <filter id='noiseFilter'>
+    <feTurbulence 
+      type='fractalNoise' 
+      baseFrequency='0.65' 
+      numOctaves='3' 
+      stitchTiles='stitch'/>
+  </filter>
+  <rect width='100%' height='100%' filter='url(#noiseFilter)'/>
+</svg>
+`
+
 export const GrainyBackground = () => {
+  const noiseBackground = `data:image/svg+xml;utf8,${encodeURIComponent(noise)}`
+
   return (
     <div
       className="pointer-events-none fixed -top-[210vh] left-1/2 isolate -z-[1] aspect-square h-[300vh] -translate-x-1/2 mix-blend-screen"
       style={{
-        filter: 'contrast(200%) brightness(200%) invert(100%)',
-        background: `radial-gradient(closest-side, #5e6148 0%, transparent 100%), url('/noise.svg')`,
+        filter: `contrast(${grainyOptions.filter.contrast}) brightness(${grainyOptions.filter.brightness}) invert(1)`,
+        background: `radial-gradient(closest-side, #5e6148, transparent), url("${noiseBackground}")`,
       }}
     />
   )
