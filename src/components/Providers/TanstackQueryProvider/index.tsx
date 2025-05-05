799386a615cc5ff1d298a2e0166d28d033c85634
fecapark <fecapark@gmail.com>
2025-05-05 17:05:55 +0900

    :wrench: queryClient에 throwOnError 옵션 추가

--- src/components/Providers/TanstackQueryProvider/index.tsx
+++ src/components/Providers/TanstackQueryProvider/index.tsx
@@ -1,13 +1,22 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      throwOnError: true,
+    },
+    mutations: {
+      throwOnError: true,
+    },
+  },
+})
 
 export const TanstackQueryProvider = ({ children }: React.PropsWithChildren<unknown>) => {
   return (
     <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
       {children}
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   )
 }
