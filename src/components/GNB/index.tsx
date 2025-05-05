cf566851400d01d3853cfbeaf6558eb6936392fd
fecapark <fecapark@gmail.com>
2025-05-06 00:05:02 +0900

    :sparkles: GNB에 프로필 띄우기

--- src/components/GNB/index.tsx
+++ src/components/GNB/index.tsx
@@ -1,11 +1,21 @@
+import { useAuth } from '@/components/Providers/AuthProvider/hook'
 import { Link } from '@tanstack/react-router'
 
 export const GNB = () => {
+  const { isAuthenticated } = useAuth()
+
   return (
     <div className="flex h-[60px] w-full shrink-0 items-center justify-between px-5">
       <Link className="flex h-10 items-center" to="/">
         <img alt="logo" className="h-4.5" src="/Logo.png" />
       </Link>
+
+      {isAuthenticated() && (
+        <div className="size-9 overflow-hidden rounded-full">
+          {/* Todo: 유저 아이디에 따라 다른 프로필 이미지 보여주기 */}
+          <img alt="profile" className="size-full object-contain" src="/profiles/1.png" />
+        </div>
+      )}
     </div>
   )
 }
--- src/routes/~_auth/~index.tsx
+++ src/routes/~_auth/~index.tsx
@@ -1,8 +1,10 @@
+import { GNB } from '@/components/GNB'
 import { createFileRoute, Link } from '@tanstack/react-router'
 
 const Index = () => {
   return (
     <div>
+      <GNB />
       <Link to="/signin">로그인</Link>
     </div>
   )
