9b98ac7501226af7e0f62008b388e164beb4d5e9
fecapark <fecapark@gmail.com>
2025-05-05 20:17:50 +0900

    :sparkles: 로그인 api 구현

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -10,6 +10,11 @@ interface SignupProps {
   password: string
 }
 
+interface SigninProps {
+  email: string
+  password: string
+}
+
 const SignupResponseSchema = z.object({
   email: z.string(),
   nickname: z.string(),
@@ -24,7 +29,19 @@ const SignupResponseSchema = z.object({
 
 type SignupResponse = z.infer<typeof SignupResponseSchema>
 
+const SigninResponseSchema = z.object({
+  access_token: z.string(),
+  token_type: z.string(),
+})
+
+type SigninResponse = z.infer<typeof SigninResponseSchema>
+
 export const signup = async (props: SignupProps) => {
   const res = await api.post<SignupResponse>('auth', { json: props }).json()
   return camelizeSchema(SignupResponseSchema).parse(res)
+}
+
+export const signin = async (props: SigninProps) => {
+  const res = await api.post<SigninResponse>('auth/login', { json: props }).json()
+  return camelizeSchema(SigninResponseSchema).parse(res)
 }
