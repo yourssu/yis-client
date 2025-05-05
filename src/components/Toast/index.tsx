ba79cbeb7ffb4fa72e282b0ef4d0a912b00ff7d0
fecapark <fecapark@gmail.com>
2025-05-06 01:37:49 +0900

    :lipstick: 토스트 > default variant 레이아웃 크기 조정

--- src/components/Toast/index.tsx
+++ src/components/Toast/index.tsx
@@ -4,11 +4,12 @@ import { ToastType } from '@/components/Providers/ToastProvider/context'
 import { DotLottieReact } from '@lottiefiles/dotlottie-react'
 
 const lottie = tv({
+  base: 'flex items-center justify-center',
   variants: {
     type: {
-      error: 'mr-1 size-7',
+      error: 'size-9 p-1',
       success: 'size-9',
-      default: 'h-9 w-0',
+      default: 'h-9 w-2.5',
     },
   },
 })
@@ -27,12 +28,9 @@ export const Toast = ({ children, type }: React.PropsWithChildren<{ type: ToastT
 
   return (
     <div className="bg-grey100 flex items-center rounded-xl py-2 pr-5.5 pl-3 font-semibold text-white">
-      <div className="flex size-9 items-center justify-center">
-        <div className={lottie({ type })}>
-          {!!src && <DotLottieReact autoplay loop={false} speed={1.5} src={src} />}
-        </div>
+      <div className={lottie({ type })}>
+        {!!src && <DotLottieReact autoplay loop={false} speed={1.5} src={src} />}
       </div>
-
       {children}
     </div>
   )
