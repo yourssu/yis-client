ee769c6e1f943cc1755ead67352cf1b024c17dc0
fecapark <fecapark@gmail.com>
2025-05-07 07:29:26 +0900

    :lipstick: 메인화면 랜딩 마크업 작성
    .

--- src/routes/~_auth/~index.tsx
+++ src/routes/~_auth/~index.tsx
@@ -1,11 +1,20 @@
 import { GNB } from '@/components/GNB'
-import { createFileRoute, Link } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router'
 
 const Index = () => {
   return (
     <div>
       <GNB />
-      <Link to="/signin">로그인</Link>
+
+      <div className="mx-auto mt-[13vh] flex flex-col items-center gap-6">
+        <img alt="server" className="mx-auto size-50" src="/Server.png" />
+        <div className="text-neutralMuted mb-6 text-xl font-semibold">
+          아직 배포된 서비스가 없어요.
+        </div>
+        <button className="bg-violet100 text-15 text-brandAdaptive hover:bg-violet200 ease-ease cursor-pointer rounded-xl px-5.5 py-2.5 font-medium transition-colors duration-200">
+          서비스 배포하기
+        </button>
+      </div>
     </div>
   )
 }
