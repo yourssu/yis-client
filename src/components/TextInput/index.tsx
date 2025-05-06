f291fb91cc48e64b8783a3cae682dc7239003497
fecapark <fecapark@gmail.com>
2025-05-06 18:15:28 +0900

    :sparkles: TextInput에 label 추가

--- src/components/TextInput/index.tsx
+++ src/components/TextInput/index.tsx
@@ -1,11 +1,12 @@
 import clsx from 'clsx'
 
-export const TextInput = ({
-  className,
-  invalid,
-  ...props
-}: React.InputHTMLAttributes<HTMLInputElement> & { invalid?: boolean }) => {
-  return (
+type TextInputProps = React.InputHTMLAttributes<HTMLInputElement> & {
+  invalid?: boolean
+  label?: string
+}
+
+export const TextInput = ({ className, invalid, label, ...props }: TextInputProps) => {
+  const input = (
     <input
       className={clsx(
         'border-grey200 focus:border-brandPrimary hover:border-brandDisabled ease-ease w-full rounded-lg border px-4 py-2 transition-colors duration-200',
@@ -15,4 +16,15 @@ export const TextInput = ({
       {...props}
     />
   )
+
+  if (label) {
+    return (
+      <div>
+        <div className="text-15 !text-neutralMuted py-1.5 font-medium">{label}</div>
+        {input}
+      </div>
+    )
+  }
+
+  return input
 }
