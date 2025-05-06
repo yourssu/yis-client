dc3c249cd97068e8412e5eb393d4723c96410d61
fecapark <fecapark@gmail.com>
2025-05-06 15:19:54 +0900

    :sparkles: ItemList 컴포넌트 구현

--- src/components/ItemList/index.tsx
+++ src/components/ItemList/index.tsx
@@ -1,4 +1,7 @@
+import { RxQuestionMarkCircled } from 'react-icons/rx'
+
 import { Divider } from '@/components/Divider'
+import { HoverTooltip } from '@/components/HoverTooltip'
 import { InlineButton } from '@/components/InlineButton'
 
 const Header = ({
@@ -24,10 +27,21 @@ const Body = ({ children }: React.PropsWithChildren<unknown>) => {
   return <div className="flex w-full flex-col">{children}</div>
 }
 
-const Item = ({ children, label }: React.PropsWithChildren<{ label: string }>) => {
+const Item = ({
+  children,
+  label,
+  tooltipContent,
+}: React.PropsWithChildren<{ label: string; tooltipContent?: string }>) => {
   return (
     <div className="text-15 flex w-full items-center justify-between py-2">
-      <div className="text-neutralMuted">{label}</div>
+      <div className="text-neutralMuted flex items-center gap-1">
+        <div>{label}</div>
+        {tooltipContent && (
+          <HoverTooltip content={tooltipContent} contentProps={{ side: 'bottom' }}>
+            <RxQuestionMarkCircled />
+          </HoverTooltip>
+        )}
+      </div>
       <div className="font-medium">{children}</div>
     </div>
   )
