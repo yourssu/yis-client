84f40a16a652303016b3b9031288e79d8d84f0c5
fecapark <fecapark@gmail.com>
2025-05-07 21:54:15 +0900

    :sparkles: 어플리케이션 스키마 작성

---
+++ src/types/application.ts
@@ -0,0 +1,23 @@
+import { z } from 'zod'
+
+import { UserResponseSchema } from '@/types/user'
+import { camelizeSchema } from '@/utils/zod'
+
+export const ApplicationResponseSchema = z.object({
+  description: z.string(),
+  id: z.number(),
+  name: z.string(),
+  user: UserResponseSchema.pick({
+    email: true,
+    nickname: true,
+    part: true,
+  }),
+  applied_deployment_id: z.number(),
+  created_at: z.string(),
+  updated_at: z.string(),
+  deleted_at: z.string().nullable(),
+})
+export type ApplicationResponseType = z.infer<typeof ApplicationResponseSchema>
+
+export const ApplicationSchema = camelizeSchema(ApplicationResponseSchema)
+export type ApplicationType = z.infer<typeof ApplicationSchema>
