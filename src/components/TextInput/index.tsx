8293f4d022bca3439e4a6707c7c55b5fab7da987
fecapark <fecapark@gmail.com>
2025-05-08 20:25:53 +0900

    :sparkles: TextInput > description 추가

--- src/components/TextInput/index.tsx
+++ src/components/TextInput/index.tsx
@@ -3,20 +3,24 @@ import clsx from 'clsx'
 import { Label } from '@/components/Label'
 
 type TextInputProps = React.InputHTMLAttributes<HTMLInputElement> & {
+  description?: string
   invalid?: boolean
   label?: string
 }
 
-export const TextInput = ({ className, invalid, label, ...props }: TextInputProps) => {
+export const TextInput = ({ className, description, invalid, label, ...props }: TextInputProps) => {
   const input = (
-    <input
-      className={clsx(
-        'border-grey200 focus:border-brandPrimary hover:border-brandDisabled ease-ease w-full rounded-lg border px-4 py-2 transition-colors duration-200',
-        invalid && '!border-negative',
-        className
-      )}
-      {...props}
-    />
+    <div className="flex flex-col gap-1.5">
+      <input
+        className={clsx(
+          'border-grey200 focus:border-brandPrimary hover:border-brandDisabled ease-ease w-full rounded-lg border px-4 py-2 transition-colors duration-200',
+          invalid && '!border-negative',
+          className
+        )}
+        {...props}
+      />
+      <div className="text-neutralSubtle text-13 font-medium">{description}</div>
+    </div>
   )
 
   if (label) {
