26ae273d4aad6948256cd38979b93b621d678cd5
fecapark <fecapark@gmail.com>
2025-05-27 03:15:43 +0900

    :sparkles: 배포 정보 입력 벨리데이션 개선

--- src/routes/~_auth/~(index)/components/CreateApplicationFunnel/DeploymentInfoFormStep.tsx
+++ src/routes/~_auth/~(index)/components/CreateApplicationFunnel/DeploymentInfoFormStep.tsx
@@ -6,6 +6,9 @@ import { Dialog } from '@/components/Dialog'
 import { NumberInput } from '@/components/TextInput/NumberInput'
 import { TextInput } from '@/components/TextInput/TextInput'
 import { DeployConfirmedContext, DeployContext } from '@/routes/~_auth/~(index)/type'
+import { assertNonNullish } from '@/utils/assertion'
+import { regexes } from '@/utils/regex'
+import { getZodErrorMessage } from '@/utils/zod'
 
 interface DeploymentInfoFormProps {
   initialValue?: DeployContext
@@ -22,32 +25,56 @@ export const DeploymentInfoFormStep = ({
   const [imageUrl, setImageUrl] = useInputState(initialValue?.imageUrl ?? '')
   const [port, setPort] = useState<number | undefined>(initialValue?.port)
   const [message, setMessage] = useInputState(initialValue?.message ?? '')
+  const [invalidText, setInvalidText] = useState<string | undefined>(undefined)
 
-  const { error } = DeploymentInfoFormSchema.safeParse({
+  const formData = {
     domain,
     imageUrl,
     port,
     message,
-  })
+  }
+
+  const { error: buttonError } = DeploymentInfoFormSchema.button.safeParse(formData)
+
+  const onClickNext = () => {
+    const { error: formError } = DeploymentInfoFormSchema.form.safeParse(formData)
+
+    if (formError) {
+      setInvalidText(getZodErrorMessage(formError))
+      return
+    }
+
+    assertNonNullish(port)
+
+    onNext({ domain, imageUrl, port, message })
+  }
 
   return (
     <>
       <Dialog.Content className="h-[440px] w-[500px]">
         <div className="flex flex-col gap-4 pb-8">
           <TextInput
-            description="ex) https://www.google.com"
+            description="http, https 없이 입력해주세요."
             onChange={setDomain}
-            placeholder="도메인"
+            placeholder="도메인 (예: www.example.com)"
             value={domain}
           />
           <NumberInput onChange={setPort} placeholder="포트 번호" value={port} />
-          <TextInput onChange={setImageUrl} placeholder="도커 이미지 링크" value={imageUrl} />
+          <TextInput
+            description="ex) alexwhen/docker-2048:latest2"
+            onChange={setImageUrl}
+            placeholder="도커 이미지 링크"
+            value={imageUrl}
+          />
           <TextInput
             description="관리자가 추가로 알아야 할 사항이 있다면 적어주세요."
             onChange={setMessage}
             placeholder="남길 메시지 (선택)"
             value={message}
           />
+          {invalidText && (
+            <div className="text-negative mx-auto text-center text-sm">{invalidText}</div>
+          )}
         </div>
       </Dialog.Content>
       <Dialog.ButtonGroup>
@@ -59,13 +86,7 @@ export const DeploymentInfoFormStep = ({
         >
           이전
         </Dialog.Button>
-        <Dialog.Button
-          disabled={!!error}
-          onClick={() => {
-            onNext({ domain, imageUrl, port: port ?? 0, message })
-          }}
-          variant="primary"
-        >
+        <Dialog.Button disabled={!!buttonError} onClick={onClickNext} variant="primary">
           다음
         </Dialog.Button>
       </Dialog.ButtonGroup>
@@ -73,9 +94,26 @@ export const DeploymentInfoFormStep = ({
   )
 }
 
-const DeploymentInfoFormSchema = z.object({
-  domain: z.string().url(),
-  port: z.number().min(0).max(65535),
-  imageUrl: z.string().url(),
-  message: z.string().optional(),
-})
+const baseValidation = {
+  domain: z.string().min(1, { message: '도메인을 입력해주세요.' }),
+  port: z.number(),
+  imageUrl: z.string().min(1, { message: '도커 이미지 링크를 입력해주세요.' }),
+}
+
+const DeploymentInfoFormSchema = {
+  button: z.object(baseValidation),
+  form: z.object({
+    domain: baseValidation.domain
+      .refine((domain) => !(domain.startsWith('http://') || domain.startsWith('https://')), {
+        message: '도메인에 http 또는 https가 포함되어 있어요.',
+      })
+      .refine((domain) => regexes.hostname.test(domain), {
+        message: '도메인 형식이 올바르지 않아요.',
+      }),
+    port: baseValidation.port
+      .min(0, { message: '포트 번호가 올바르지 않아요.' })
+      .max(65535, { message: '포트 번호가 올바르지 않아요.' }),
+    imageUrl: baseValidation.imageUrl,
+    message: z.string().optional(),
+  }),
+}
